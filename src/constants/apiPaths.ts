7c543bfe8a6af44f0afcf8538fd10cdaf5c4a564
anjali-sim <anjalighetia123@gmail.com>
2024-07-21 15:17:14 +0530

    feat: changed the url name

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,11 +2,10 @@ export const API_PATHS = {
   signup: 'https://bytebuilders-api.cloudjet.site/api/register',
   login: 'https://bytebuilders-api.cloudjet.site/api/login_check',
   logout: 'https://bytebuilders-api.cloudjet.site/api/logout',
-  addPreferences:
-    'https://bytebuilders-api.cloudjet.site/user/preferences/add',
+  addPreferences: 'https://bytebuilders-api.cloudjet.site/user/preferences/add',
   editPreferences:
     'https://bytebuilders-api.cloudjet.site/api/user/preferences/edit',
-    getRecipes: 'http://172.16.3.68:8000/api/recipe/all',
-    getSingleRecipe: 'http://172.16.3.68:8000/api/recipe',
+  getRecipes: 'http://172.16.3.68:8000/api/recipe/all',
+  getSingleRecipe: 'http://172.16.3.68:8000/api/recipe',
   getMealPlan: 'https://bytebuilders-api.cloudjet.site/meal/all'
 }
